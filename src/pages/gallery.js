01e7a74ba217c7a1b45a26872749f9a94a127151
Jonathan Verduzco <96271886+jon-verd@users.noreply.github.com>
2022-05-31 19:02:52 -0600

    Was able to integrete react bootstrap and Create a new Gallery page

---
+++ src/pages/gallery.js
@@ -0,0 +1,18 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+const GalleryPage = () => {
+    return 
+    (
+      <>
+
+        <h1> Hi, Welcome to our gallery page.</h1>
+
+      </>
+    )
+}
+
+export default GalleryPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,148 +1,81 @@
-import * as React from "react"
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Col, Row, Carousel } from "react-bootstrap"
+import { Link } from "gatsby"
 
-// // styles
-// const pageStyles = {
-//   color: "#232129",
-//   padding: 96,
-//   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-// }
-// const headingStyles = {
-//   marginTop: 0,
-//   marginBottom: 64,
-//   maxWidth: 320,
-// }
-// const headingAccentStyles = {
-//   color: "#663399",
-// }
-// const paragraphStyles = {
-//   marginBottom: 48,
-// }
-// const codeStyles = {
-//   color: "#8A6534",
-//   padding: 4,
-//   backgroundColor: "#FFF4DB",
-//   fontSize: "1.25rem",
-//   borderRadius: 4,
-// }
-// const listStyles = {
-//   marginBottom: 96,
-//   paddingLeft: 0,
-// }
-// const listItemStyles = {
-//   fontWeight: 300,
-//   fontSize: 24,
-//   maxWidth: 560,
-//   marginBottom: 30,
-// }
 
-// const linkStyle = {
-//   color: "#8954A8",
-//   fontWeight: "bold",
-//   fontSize: 16,
-//   verticalAlign: "5%",
-// }
-
-// const docLinkStyle = {
-//   ...linkStyle,
-//   listStyleType: "none",
-//   marginBottom: 24,
-// }
-
-// const descriptionStyle = {
-//   color: "#232129",
-//   fontSize: 14,
-//   marginTop: 10,
-//   marginBottom: 0,
-//   lineHeight: 1.25,
-// }
-
-// const docLink = {
-//   text: "Documentation",
-//   url: "https://www.gatsbyjs.com/docs/",
-//   color: "#8954A8",
-// }
-
-// const badgeStyle = {
-//   color: "#fff",
-//   backgroundColor: "#088413",
-//   border: "1px solid #088413",
-//   fontSize: 11,
-//   fontWeight: "bold",
-//   letterSpacing: 1,
-//   borderRadius: 4,
-//   padding: "4px 6px",
-//   display: "inline-block",
-//   position: "relative",
-//   top: -2,
-//   marginLeft: 10,
-//   lineHeight: 1,
-// }
-
-// // data
-// const links = [
-//   {
-//     text: "Tutorial",
-//     url: "https://www.gatsbyjs.com/docs/tutorial/",
-//     description:
-//       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-//     color: "#E95800",
-//   },
-//   {
-//     text: "How to Guides",
-//     url: "https://www.gatsbyjs.com/docs/how-to/",
-//     description:
-//       "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-//     color: "#1099A8",
-//   },
-//   {
-//     text: "Reference Guides",
-//     url: "https://www.gatsbyjs.com/docs/reference/",
-//     description:
-//       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-//     color: "#BC027F",
-//   },
-//   {
-//     text: "Conceptual Guides",
-//     url: "https://www.gatsbyjs.com/docs/conceptual/",
-//     description:
-//       "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-//     color: "#0D96F2",
-//   },
-//   {
-//     text: "Plugin Library",
-//     url: "https://www.gatsbyjs.com/plugins",
-//     description:
-//       "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-//     color: "#8EB814",
-//   },
-//   {
-//     text: "Build and Host",
-//     url: "https://www.gatsbyjs.com/cloud",
-//     badge: true,
-//     description:
-//       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-//     color: "#663399",
-//   },
-// ]
-
-// markup
 const IndexPage = () => {
   return (
     <>
       <title> Vicios </title>
-      
-      <h1> {'Vicios >:)'} </h1> 
-      <h2> 
-       <br/> This site serves two purposes: 
-       <br/> 1. For me to practice my skills as a developer and experiment in web development.
-       <br/> 2. To build the foundation of the Vicios website.
-      </h2>
+      <Container className={"d-flex flex-md-column"}>
+        <div >
+          <h1 className={"text-center m-2 p-3 display-1 fluid"}> {'Vicios >:)'} </h1> 
+        </div>
+      </Container> 
+
+      <Container className="d-flex h-75% mx-auto p-3">
+        <Carousel variant="dark" className="d-flex h-75%">
+          <Carousel.Item>
+            <img
+              className="w-100 h-20 img-flex"
+              src="https://via.placeholder.com/1500x500"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h5>First slide label</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 h-20 img-flex"
+              src="https://via.placeholder.com/1500x500"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h5>Second slide label</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 h-20"
+              src="https://via.placeholder.com/1500x500"
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h5>Third slide label</h5>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+
+      <Container className="p-3 m-3">
+      <Link to="/gallery/"> Gallery </Link>
+      </Container>
+
+      <Container className="mx-auto p-3">
       <h3>
         What is Vicios?
       </h3>
       <p>
         Vicios is just an idea for a brand a friend and I have been discussing for a long time. That idea has ranged from being a clothing brand, a furniture brand, a dispensary and many other things. At the end of the day we just want to make cool shit and get paid for it like everybody else. This jsut happens to be the way in which we envision that to happen.
       </p>
+      </Container>
+
+      <Container className="mx-auto p-3">
+        <Col className="h4 text-center align-items-center"> 
+          <br/><div className="h2 p-2">This site serves two purposes: </div>
+        <Row>
+          <br/> ○ For me to practice my skills as a developer and experiment in web development.
+        </Row>
+        <Row>
+          <br/> ○ To build the foundation of the Vicios website.
+        </Row>
+        </Col>
+      </Container>
       
     </>
   )
